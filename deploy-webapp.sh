c4e29fb12d0ad6a29425c0952fb0693ea4a2a169
root <root@ip-172-31-18-48.ap-southeast-1.compute.internal>
2019-05-14 03:52:12 +0000

    hhhh

--- deploy-webapp.sh
+++ deploy-webapp.sh
@@ -4,7 +4,7 @@
 
 echo "=== Kill Running Containers ==="
 #docker kill `docker ps | grep $1 | awk '{print $1}'`
-for i in `docker ps -aq`; do ; docker kill $i; done
+for i in `docker ps -qa` ; do docker container rm $i; done
 
 echo " ==== Build the WebApp war file ceraton"
 mvn clean package
